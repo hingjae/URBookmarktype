7d84c6d7db71b6c84949b4994f2588f21174bd15
hingjae <ljh23270@naver.com>
2024-01-02 19:20:33 +0900

    cors config

--- src/main/java/com/example/bookbti/config/WebConfig.java
+++ src/main/java/com/example/bookbti/config/WebConfig.java
@@ -10,8 +10,7 @@ public class WebConfig implements WebMvcConfigurer {
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**")
-                .allowedOrigins("http://localhost:3000")
-                .allowedOrigins("https://5eq35gl9darcd.swygbro.xn--com-of0o")
+                .allowedOrigins("http://localhost:3000", "https://5eq35gl9darcd.swygbro.xn--com-of0o")
                 .allowedMethods("GET", "POST", "PUT", "DELETE");
     }
 }
