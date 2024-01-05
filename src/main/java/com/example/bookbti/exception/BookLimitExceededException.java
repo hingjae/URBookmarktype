5c09b224209037f6a83b545faaff51ea95b1fa95
hingjae <ljh23270@naver.com>
2024-01-05 20:24:06 +0900

    에러 메세지 수정

--- src/main/java/com/example/bookbti/exception/BookLimitExceededException.java
+++ src/main/java/com/example/bookbti/exception/BookLimitExceededException.java
@@ -1,7 +1,7 @@
 package com.example.bookbti.exception;
 
 public class BookLimitExceededException extends RuntimeException {
-    private static final String MESSAGE = "이미 존재하는 아이디 입니다.";
+    private static final String MESSAGE = "책은 최대 3권까지 고를 수 있습니다.";
 
     public BookLimitExceededException() {
         super(MESSAGE);
